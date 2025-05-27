9fce75b5b5539dc0aa1f381fc41a8ea2f6b1772f
阿飞 <199179549+afei4gh@users.noreply.github.com>
2025-05-27 13:37:35 +0800

    feat: init

---
+++ src/main/java/io/github/rest4j/core/Constants.java
@@ -0,0 +1,8 @@
+package io.github.rest4j.core;
+
+public final class Constants {
+
+    private Constants() {
+        throw new UnsupportedOperationException("This is a constant class and cannot be instantiated");
+    }
+}
